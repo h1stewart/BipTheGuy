15b326a8cd3a6ef1a9f95374dbc619967cbc9071
Hailey Stewart <kickinhailey@icloud.com>
2023-02-14 17:35:07 -0500

    BipGuy

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,21 +6,69 @@
 //
 
 import SwiftUI
+import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
+    @State private var selectedPhoto: PhotosPickerItem? //an optional, meaning it could be nil
+    @State private var bipImage = Image("clown")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            bipImage
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    animateImage = false
+                    withAnimation (.spring(response: 0.3, dampingFraction: 0.3)) {
+                        animateImage = true // 90% to 100% using spring animation
+                    }
+                }
+            
+            
+            Spacer()
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            } //can add button styles to photo pickers
+            .onChange(of: selectedPhoto) { newValue in
+                Task{
+                    do {
+                        if let data = try await newValue?.loadTransferable(type: Data.self) {
+                            if let uiImage = UIImage(data: data){
+                                bipImage = Image(uiImage: uiImage)
+                            }
+                        }
+                    } catch{print("😡ERROR: loading failed \(error.localizedDescription)") // a function that throws an error will give you a value **DOES NOT WORK WITH try?
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else{
+            print("😡 Could not read file name \(soundName)")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer.")
+        }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+    
